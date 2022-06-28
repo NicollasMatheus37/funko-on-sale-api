3c744bfa5f58b1d20fd9e609cef51f253e1cf2fe
Vinicios Santos <95624273+vinicios-santos@users.noreply.github.com>
2022-06-28 13:30:53 -0300

    Add files via upload

--- src/pages/store/index.jsx
+++ src/pages/store/index.jsx
@@ -1,9 +1,4 @@
-import { useEffect, useState } from "react";
-import { getAllSpaceNews } from "../../helpers/spaceNewsService";
-import { Link, useNavigate } from "react-router-dom";
-import Button from "@mui/material/Button";
 
-import "./newsletter.css";
 
 const Store = () => {
 
